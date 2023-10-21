3f8a7f3a1ab79967c118f4a67502ef424f210208
nomnivore <6979410+nomnivore@users.noreply.github.com>
2023-10-21 03:01:36 -0400

    add link to register

--- packages/client/src/pages/home.tsx
+++ packages/client/src/pages/home.tsx
@@ -1,7 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "@tanstack/react-router";
+
 export function Home() {
   // if not signed in, redirect to /register
 
-  return <div>App Index</div>;
+  return (
+    <div className="px-4">
+      <Button asChild variant="outline" size="lg" className=" my-6 text-lg">
+        <Link to="/register">Please Login to get started</Link>
+      </Button>
+    </div>
+  );
 }
 
 export default Home;
